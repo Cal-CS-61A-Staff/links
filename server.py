1df79f05ef62c35bcbbb725b951fa10aeb24c05b
Jen Thakar <jathak@gmail.com>
2017-10-26 02:00:00 -0700

    Add shortlinker

--- server.py
+++ server.py
@@ -1,10 +1,47 @@
 from flask import Flask, redirect
+import requests
+import csv
+import os
 
 app = Flask(__name__)
 
+links = {}
+author = {}
+
+@app.route('/<path>/')
+def handler(path):
+    if not links:
+        refresh()
+    if path in links:
+        return redirect(links[path])
+    return base()
+
+@app.route('/preview/<path>/')
+def preview(path):
+    if not links:
+        refresh()
+    if path not in links:
+        return 'No such link exists.'
+    return 'Points to <a href="{0}">{0}</a> by {1}'.format(links[path], author[path])
+
 @app.route("/")
-def hello():
+def base():
     return redirect('https://cs61a.org')
+
+@app.route("/_refresh/")
+def refresh():
+    csv_lines = requests.get(os.getenv('DB_URL')).text.split("\n")
+    csvr = list(csv.reader(csv_lines, delimiter=',', quotechar='"'))
+    headers = [x.lower() for x in csvr[0]]
+    links.clear()
+    author.clear()
+    for row in csvr[1:]:
+        shortlink = row[headers.index('shortlink')]
+        url = row[headers.index('url')]
+        creator = row[headers.index('creator')]
+        links[shortlink] = url
+        author[shortlink] = creator
+    return 'Links updated'
 
 if __name__ == "__main__":
     app.run()
