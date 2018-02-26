792574d97c352228f4a555c5c087d6955cb85354
Jen Thakar <jathak@gmail.com>
2018-02-26 00:22:40 -0800

    Change multi sheet support to use BASE and SHEETS params

--- server.py
+++ server.py
@@ -5,8 +5,7 @@ import os
 
 app = Flask(__name__)
 
-links = {}
-author = {}
+links, author = {}, {}
 
 @app.route('/<path>/')
 def handler(path):
@@ -30,19 +29,19 @@ def base():
 
 @app.route("/_refresh/")
 def refresh():
-    csv_lines = []
-    for url in os.getenv('DB_URL').split(','):
-        csv_lines += requests.get(os.getenv('DB_URL')).text.split("\n")
-    csvr = list(csv.reader(csv_lines, delimiter=',', quotechar='"'))
-    headers = [x.lower() for x in csvr[0]]
-    new_links, new_author = {}, {}
-    for row in csvr[1:]:
-        shortlink = row[headers.index('shortlink')]
-        url = row[headers.index('url')]
-        creator = row[headers.index('creator')]
-        new_links[shortlink] = url
-        new_author[shortlink] = creator
-    links = new_links, author = new_author
+    links.clear()
+    author.clear()
+    for sheet_id in os.getenv('SHEETS').split(','):
+        url = os.getenv('BASE_URL') + sheet_id
+        csv_lines = requests.get(url).text.split("\n")
+        csvr = list(csv.reader(csv_lines, delimiter=',', quotechar='"'))
+        headers = [x.lower() for x in csvr[0]]
+        for row in csvr[1:]:
+            shortlink = row[headers.index('shortlink')]
+            url = row[headers.index('url')]
+            creator = row[headers.index('creator')]
+            links[shortlink] = url
+            author[shortlink] = creator
     return 'Links updated'
 
 if __name__ == "__main__":
